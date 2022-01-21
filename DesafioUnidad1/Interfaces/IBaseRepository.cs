e675eead0e05185b45125c244f12e97f5d700ad3
julieta sosa <juli20002010@live.com>
2022-01-21 15:48:30 -0300

    create base repository

---
+++ DesafioUnidad1/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioUnidad1.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int id);
+    }
+}
