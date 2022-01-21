b5e325511187e35da22ca1323edca15bf30fff7c
julieta sosa <juli20002010@live.com>
2022-01-21 15:47:38 -0300

    create base entity

---
+++ DesafioUnidad1/Interfaces/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioUnidad1.Interfaces
+{
+    public interface IBaseEntity
+    {
+        int Id { get; set; }
+    }
+}
